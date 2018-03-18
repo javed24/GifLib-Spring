a1bde2dc524d86b9e5a815e596e155633eb9b481
javed24 <aquib_javed@live.com>
2018-03-18 17:54:48 -0400

    setup complete, working on mapping

---
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -0,0 +1,19 @@
+package com.teamtreehouse.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+    @RequestMapping(value = "/")
+    @ResponseBody
+    public String listGifs(){
+        return "list of all the gifs in the world";
+    }
+    @RequestMapping(value = "/gif")
+    @ResponseBody
+    public String showGif(){
+        return "here's the gif you clicked on!";
+    }
+}
