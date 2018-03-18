de226a194aaafcb1ef5372a98a79e664b9dc778a
javed24 <aquib_javed@live.com>
2018-03-18 18:01:56 -0400

    added thymeleaf

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -7,18 +7,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GifController {
     @RequestMapping(value = "/")
-    @ResponseBody
     public String listGifs(){
-        return "list of all the gifs in the world";
+        return "home";
     }
     @RequestMapping(value = "/gif")
     @ResponseBody
     public String showGif(){
         return "here's the gif you clicked on!";
-    }
-    @RequestMapping(value = "/about")
-    @ResponseBody
-    public String about(){
-        return "here's the about page for the app";
     }
 }
